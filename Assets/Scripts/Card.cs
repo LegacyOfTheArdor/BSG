a7ef0f9d044c062fa18e65509f2ed3ac2a507aec
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2024-02-29 12:17:44 +0100

    init
    init commit + start on script

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+	private int health = 0;
+	private int speed = 0;
+	private int cost = 0;
+	private int damage = 0;
+
+	public int sethealth;
+	public int setspeed;
+	public int setcost;
+	public int setdamage;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
